4bcf3095867b2da48653c2d6cc5301f2867efdc7
Abdul Basith <mohammadabdulbasith123@gmail.com>
2025-07-23 00:05:48 +0530

    added react-pdf and sign func

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      external: ['pdfjs-dist/build/pdf.worker.min.js']
+    }
+  }
 });
